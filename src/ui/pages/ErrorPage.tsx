8d88d5482995c482a7cb8acec3c0c3c1422016f0
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-11 12:32:14 +0545

    changes: css

--- src/ui/pages/ErrorPage.tsx
+++ src/ui/pages/ErrorPage.tsx
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
-import '../css/Errorpage.css'
+import "../css/Errorpage.css";
+import styled from 'styled-components'
 
 function ErrorPage() {
   return (
     <>
       <div className="container">
-        <h3>404 Error</h3>
-        <p>Page not Found</p>
-        <p>
-          Get back to <Link to="/">Home Page</Link>
+        <h3 className="center">404 Error</h3>
+        <p className="center">Page not Found</p>
+        <p className="center">
+          Get back to <StyledLink to="/">Home Page</StyledLink>
         </p>
       </div>
     </>
   );
 }
+
+const StyledLink = styled(Link)`
+  color: red;
+  text-decoration: none;
+  margin: 1rem;
+  position: relative;
+  font-weight: bold;
+  cursor: default;
+`;
 
 export default ErrorPage;
