253737be37c44353e2f2b05388aa4ecb505104bc
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-11 12:11:09 +0545

    changes: css

--- src/ui/pages/ErrorPage.tsx
+++ src/ui/pages/ErrorPage.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
-
+import '../css/Errorpage.css'
 
 function ErrorPage() {
   return (
     <>
-      <h3>404 Error</h3>
-      <p>Page not Found</p>
-      <p>Get back to <Link to='/'>Home Page</Link></p>
+      <div className="container">
+        <h3>404 Error</h3>
+        <p>Page not Found</p>
+        <p>
+          Get back to <Link to="/">Home Page</Link>
+        </p>
+      </div>
     </>
   );
 }
 
-export default ErrorPage
+export default ErrorPage;
