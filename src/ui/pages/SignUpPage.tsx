e660a7d3e2ca4a0789e01f7ba636f7e9d2adc438
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-13 10:58:16 +0545

    signup form

--- src/ui/pages/SignUpPage.tsx
+++ src/ui/pages/SignUpPage.tsx
@@ -1,9 +1,62 @@
+import { Box, TextField, Typography, styled } from "@mui/material";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
-
+interface IFormInputs {
+  fullName: string;
+  email: string;
+  password: string;
+}
 function SignUpPage() {
+  const { control, handleSubmit } = useForm<IFormInputs>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSignUp:SubmitHandler<IFormInputs> = (data)=>{
+    console.log(data);
+    
+  }
   return (
-    <div>SignUpPage</div>
-  )
+    <>
+      <Container>
+        <Typography>Sign Up here</Typography>
+        <form onSubmit={handleSubmit(onSignUp)}>
+          <Controller
+          name="fullName"
+          control={control}
+          render={({field})=>(
+            <TextField {...field}  placeholder="Full Name" type="text"/>
+          )}
+          />
+          <Controller
+          name="email"
+          control={control}
+          render={({field})=>(
+            <TextField {...field} placeholder="email" type="email" />
+          )}
+          />
+          <Controller
+          name="password"
+          control={control}
+          render={({field})=>(
+            <TextField {...field} placeholder="password" type="password" />
+          )}
+          />
+        </form>
+      </Container>
+    </>
+  );
 }
 
-export default SignUpPage
+const Container = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  
+`;
+
+export default SignUpPage;
