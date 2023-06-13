a21b200bf007c3244a4a253e03a8a24cf2bf3cd3
Sujeet Mainali <mainalisujeet@gmail.com>
2023-06-13 13:58:43 +0545

    changes

--- src/ui/pages/SignUpPage.tsx
+++ src/ui/pages/SignUpPage.tsx
@@ -22,30 +22,51 @@ function SignUpPage() {
   return (
     <>
       <Container>
-        <Typography>Sign Up here</Typography>
+        <Typography style={{fontSize:20, fontWeight:'bolder'}}>Sign Up here</Typography>
         <form onSubmit={handleSubmit(onSignUp)}>
           <Controller
-          name="fullName"
-          control={control}
-          render={({field})=>(
-            <TextField {...field}  placeholder="Full Name" type="text"/>
-          )}
+            name="fullName"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                placeholder="Full Name"
+                type="text"
+                style={{ margin: 10 }}
+              />
+            )}
           />
           <Controller
-          name="email"
-          control={control}
-          render={({field})=>(
-            <TextField {...field} placeholder="email" type="email" />
-          )}
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                placeholder="email"
+                type="email"
+                style={{ margin: 10 }}
+              />
+            )}
           />
           <Controller
-          name="password"
-          control={control}
-          render={({field})=>(
-            <TextField {...field} placeholder="password" type="password" />
-          )}
+            name="password"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                placeholder="password"
+                type="password"
+                style={{ margin: 10 }}
+              />
+            )}
           />
-          <Button type="submit" variant="outlined" >Sign Up </Button>
+          <Button
+            type="submit"
+            variant="outlined"
+            style={{ width: 240, marginLeft: 10 }}
+          >
+            Sign Up{" "}
+          </Button>
         </form>
       </Container>
     </>
@@ -56,8 +77,12 @@ const Container = styled(Box)`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 
-
+form{
+  display: flex;
+  flex-direction: column;
+}
 `;
 
 export default SignUpPage;
